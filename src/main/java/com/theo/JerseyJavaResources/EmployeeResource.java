d44de5b36027473f8efd2845051af809da6c0cae
ashishdutta007 <ashish.dutta050@gmail.com>
2017-02-21 18:10:03 +0530

    Basic Maven setup for jersey-hibernate project

---
+++ src/main/java/com/theo/JerseyJavaResources/EmployeeResource.java
@@ -0,0 +1,22 @@
+package com.theo.JerseyJavaResources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author DuttaAsC
+ * Jersey framework is an implementation of JAX-RS with additional features.
+ * EmployeeResource class is an implementation of JAX-RS resource.
+ * Path identifies class as a service class.
+ */
+@Path("/resource")
+public class EmployeeResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String welcomeMessage() {
+		return "Hello Employee !!!";
+	}
+}
